60c751dfb77513fe29d1379948560030aea06d41
liapetelko <98822243+liapetelko@users.noreply.github.com>
2022-02-08 00:23:41 +0200

    Add files via upload

---
+++ lia.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+#First Questions
+start = int(st.selectbox("Enter the start", [10,11,12,13,14,15,16,17,18,19,20]))
+end = int(st.selectbox("Enter the end", [10,11,12,13,14,15,16,17,18,19,20]))
+adults = int(st.selectbox("Adults?",[1,2,3,4]))
+
+kidsBool = st.button("Press if wanna add Kids / Babies")
+if (kidsBool):
+    kids = int(st.selectbox("Kids?")[0,1,2,3,4])
+    babies = int(st.selectbox("Babies?")[0,1,2,3,4])
+
+hotel = st.selectbox("Hotel?", ["RB","KS","SP","LAG","RIV","ROG"])
+
+#Some upgrades..
